0895b0a0b7b77afc1cacd918bff7ac5fa6a05d47
Tiago Gonçalves <a29389@alunos.ipca.pt>
2026-04-22 19:33:17 +0100

    adicionados comentários no ficheiro marketplaceRoutes.js

--- backend/routes/marketplaceRoutes.js
+++ backend/routes/marketplaceRoutes.js
@@ -17,12 +17,19 @@ const authMiddleware = require("../middleware/authMiddleware");
 const perfilMiddleware = require("../middleware/perfilMiddleware");
 
 // Routes para marketplace
+// criar novo anuncio
 route.post("/", authMiddleware, perfilMiddleware(["ALUNO"]), controller.newMarketplace);
+// obter anuncios do marketplace
 route.get("/", authMiddleware, controller.getMarketplace);
+// visualizar anuncios do marketplace para gestao (todos os anuncios, incluindo os rejeitados e arquivados)
 route.get("/gestao", authMiddleware, perfilMiddleware(["FUNCIONARIO", "ADMIN"]), controller.getMarketplaceGestao);
+// obter anuncio do marketplace por id (visualizar histórico de um anuncio, incluindo os rejeitados e arquivados, sendo que o aluno só pode visualizar os seus anuncios)
 route.get("/:userid", authMiddleware, controller.getMarketplaceById);
+// editar anuncio do marketplace (apenas se estiver pendente)
 route.patch("/:id", authMiddleware, perfilMiddleware(["ALUNO"]), controller.editMarketplace);
+// aprovar ou rejeitar anuncio do marketplace (apenas se estiver pendente)
 route.patch("/:id/aprovar", authMiddleware, perfilMiddleware(["FUNCIONARIO", "ADMIN"]), controller.updateMarketplaceStatus);
+// mudar para arquivado um anuncio do marketplace (apenas se estiver aprovado)
 route.delete("/:id", authMiddleware, perfilMiddleware(["ALUNO"]), controller.deleteMarketplace);
 
 // Exportar routes
